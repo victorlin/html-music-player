665e4ad73274f09566adc221c02d6eba514a96ac
Victor Lin <13424970+victorlin@users.noreply.github.com>
2023-12-28 21:38:44 -0800

    Add service worker
    https://developer.mozilla.org/en-US/docs/Web/API/CacheStorage/open#examples
    https://developer.mozilla.org/en-US/docs/Web/API/FetchEvent/respondWith#examples

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open('v1')
+      .then((cache) =>
+        cache.addAll([
+          '/',
+          '/index.html',
+        ]),
+      ),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    (async () => {
+      const cachedResponse = await caches.match(event.request);
+      if (cachedResponse) return cachedResponse;
+      return fetch(event.request);
+    })(),
+  );
+});
